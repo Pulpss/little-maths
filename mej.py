8490c77b9993755fda59e7f97066b5ff1370e8c6
Pulpss <pulpsyt@gmail.com>
2022-04-04 23:53:41 +0200

    Update mej.py

--- mej.py
+++ mej.py
@@ -5,9 +5,6 @@ cols = int(input("Donner valeur col: "))
 
 x = VarArray(size=[rows, cols], dom={0, 1})
 
-for i in range(1,2):
-    print(i)
-
 satisfy(
     [(x[col-1, row-1]+x[col, row-1]+x[col+1, row-1]+x[col-1, row]+x[col+1, row]+x[col-1, row+1]+x[col, row+1]+x[col+1, row+1])%2 == 1 for row in range(1, cols-1) for col in range(1, rows-1)],
     [(x[0, 1]+x[1, 0]+x[1, 1])%2 == 1],
@@ -24,5 +21,3 @@ if solve(sols=ALL) is SAT:
     for i in range(rows):
         print(values(x[i]))
     print("Number of solutions: ", n_solutions())
-
-print("Domain of any variable: ", x[0][0].dom)
